a3b5e18945ea1fc7a5f47c2412a02ea0b7b7a964
cryoemadmin <cryoemadmin@luke.cnio.es>
2025-03-25 18:52:37 +0100

    comeon

--- config.sh
+++ config.sh
@@ -1,14 +1,14 @@
 #Manually set the paths for every other software you have installed and environment name
 
-RFD_PATH="/path/to/RFDiffusion"
-PMPNN_PATH="/path/to/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as SILENT and AF2IG
-AF2IG_PATH="/path/to/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as PMPNN and SILENT
+RFD_PATH="/apps/rosetta/RFDiffusion"
+PMPNN_PATH="/apps/rosetta/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as SILENT and AF2IG
+AF2IG_PATH="/apps/rosetta/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as PMPNN and SILENT
 MICRORUN_PATH="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-SILENT_PATH="/path/to/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as PMPNN and AF2IG
+SILENT_PATH="/apps/rosetta/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as PMPNN and AF2IG
 
 # MANUALLY SET ENVIRONMENTS NAMES (example names set)
 
-RFD_ENV="SE3nv"
+RFD_ENV="SE3nv4090"
 PMPNN_ENV="dl_binder_design"
 AF2_ENV="dl_binder_design"
 MICRORUN_ENV="watcher"
@@ -17,6 +17,6 @@ MICRORUN_ENV="watcher"
 #MANUALLY SET SBATCH CONFIGURATIONS (examples in place)
 
 NODES=1
-PARTITION=Binder_design
+PARTITION=RFD
 CPUS_PER_GPU=12
 GRES=gpu:1
