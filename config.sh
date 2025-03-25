6777d0856c52611da73762a9349ab3f3d7b991ec
carloschacon-cell <carloschacon2121@gmail.com>
2025-03-25 19:35:41 +0100

    jooder

---
+++ config.sh
@@ -0,0 +1,25 @@
+#Manually set the paths for every other software you have installed and environment name
+
+RFD_PATH="/apps/rosetta/RFDifussion"
+PMPNN_PATH="/apps/rosetta/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as SILENT and AF2IG
+AF2IG_PATH="/apps/rosetta/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as PMPNN and SILENT
+MICRORUN_PATH="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+SILENT_PATH="/apps/rosetta/dl_binder_design" #If you are usign the tools from nrbennett repo, it should be the same as PMPNN and AF2IG
+
+# MANUALLY SET ENVIRONMENTS NAMES (example names set)
+
+RFD_ENV="SE3nv4090"
+PMPNN_ENV="dl_binder_design"
+AF2_ENV="dl_binder_design"
+MICRORUN_ENV="watcher"
+
+
+#MANUALLY SET SBATCH CONFIGURATIONS (examples in place)
+
+NODES=1
+PARTITION=RFD
+CPUS_PER_GPU=12
+GRES=gpu:1
+
+
+source /apps/profile.d/load_all.sh
